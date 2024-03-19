55b50b155e8f4054b1b057bff43d8d911c5296a7
Benjamin Janssens <benji.janssens@gmail.com>
2024-03-19 19:55:56 +0100

    init: set up config

---
+++ src/app.config.ts
@@ -0,0 +1,16 @@
+import { validateConfig, Environment } from '@libs/common';
+import { registerAs } from '@nestjs/config';
+import { Expose } from 'class-transformer';
+import { IsEnum, IsPort } from 'class-validator';
+
+export class AppConfig {
+  @Expose({ name: 'NODE_ENV' })
+  @IsEnum(Environment)
+  environment: Environment;
+
+  @Expose({ name: 'PORT' })
+  @IsPort()
+  port: string;
+}
+
+export const appConfig = registerAs('app', () => validateConfig(AppConfig));
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppConfig, appConfig } from './app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const _appConfig: AppConfig = await app.get(appConfig.KEY);
+
+  await app.listen(_appConfig.port);
 }
+
 bootstrap();
