38f05c26d81f5ddb80ef36d503bfd9799402dbe3
Benjamin Janssens <benji.janssens@gmail.com>
2024-03-19 20:10:03 +0100

    init: add versioning; add request validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 import { AppConfig, appConfig } from './app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const _appConfig: AppConfig = await app.get(appConfig.KEY);
+
+  // Versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  // Request validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(_appConfig.port);
 }
